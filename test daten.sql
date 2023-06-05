91157f941152c536aae60b4efc9d1c88753a9ca8
Niklas <niklas.weiand@stud.h-da.de>
2023-06-05 12:44:19 +0200

    database tests

--- test daten.sql
+++ test daten.sql
@@ -1,3 +1,6 @@
 insert into users(id, username, punktstand)
 values(default, 'Niklas', default);
-insert into spiele(id, spieltag, )
+insert into spiele(id, spieltag, heimteam, auswärtsteam, heimtore, auswärsttore)
+values(default, 1, 'BVB','Bayern', 2,0);
+insert into tipps(userid, spielid, tippheimtore, tippauswärtstore, punkte)
+values (1,1,2,1,NULL);
